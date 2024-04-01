b5b838bcb35db7ae3f8821f5772c71e7d43f9973
ryanmerolle <ryan.merolle@gmail.com>
2024-04-01 17:34:56 +0000

    temp bump ssh login timers

--- netcfgbu/connectors/ssh.py
+++ netcfgbu/connectors/ssh.py
@@ -6,12 +6,12 @@ class LoginPromptUserPass(BasicSSHConnector):
     async def login(self):
         await super(LoginPromptUserPass, self).login()
 
-        await asyncio.wait_for(self.process.stdout.readuntil(b"User:"), timeout=10)
+        await asyncio.wait_for(self.process.stdout.readuntil(b"User:"), timeout=60)
 
         username = (self.conn_args["username"] + "\n").encode("utf-8")
         self.process.stdin.write(username)
 
-        await asyncio.wait_for(self.process.stdout.readuntil(b"Password:"), timeout=10)
+        await asyncio.wait_for(self.process.stdout.readuntil(b"Password:"), timeout=60)
 
         password = (self.conn_args["password"] + "\n").encode("utf-8")
         self.process.stdin.write(password)
